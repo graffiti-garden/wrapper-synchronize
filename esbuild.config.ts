95484bd7c19452169d4c7eba716a9fecfaf18743
theia <theia@mit.edu>
2025-02-04 18:35:14 -0500

    esbuild over rollup

---
+++ esbuild.config.ts
@@ -0,0 +1,27 @@
+import * as esbuild from "esbuild";
+import { polyfillNode } from "esbuild-plugin-polyfill-node";
+
+await esbuild.build({
+  entryPoints: ["src/index.ts"],
+  platform: "browser",
+  bundle: true,
+  sourcemap: true,
+  minify: true,
+  format: "esm",
+  target: "esnext",
+  outfile: "dist/index.browser.js",
+  plugins: [polyfillNode()],
+});
+
+for (const format of ["esm", "cjs"] as const) {
+  await esbuild.build({
+    entryPoints: ["src/*"],
+    platform: "neutral",
+    sourcemap: true,
+    minify: true,
+    format,
+    target: "esnext",
+    outdir: `dist/${format}`,
+    treeShaking: true,
+  });
+}
