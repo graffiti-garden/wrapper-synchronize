eeffab483aa45af1ec5a29e49e433986ce52df2e
theia <theia@mit.edu>
2025-02-10 15:06:08 -0500

    remove non-sync files

--- src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./1-api";
-export * from "./2-types";
-export * from "./3-errors";
-export type { JSONSchema4 } from "json-schema";
--- tests/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./location";
-export * from "./crud";
-export * from "./synchronize";
-export * from "./discover";
-export * from "./orphans";
-export * from "./channel-stats";
